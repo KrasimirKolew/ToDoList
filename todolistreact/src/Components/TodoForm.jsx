899d4367175373313eb182f9fa00282f60ac25ee
Krasimir Kolev <krasirumenov789@gmail.com>
2024-05-09 18:32:47 +0300

    change form

--- todolistreact/src/Components/TodoForm.jsx
+++ todolistreact/src/Components/TodoForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 export const TodoForm = ({  addTodo }) => {
-    const [value,setValue]=useState("")
+    const [value,setValue]=useState('')
 
     const handleSubmit = e => {
         e.preventDefault();
