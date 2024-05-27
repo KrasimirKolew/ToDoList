84d881e547c0c98581476e40089f01b74c3c0c23
Krasimir Kolev <krasirumenov789@gmail.com>
2024-05-27 22:39:58 +0300

    finish

--- todolistreact/src/Components/Todo.jsx
+++ todolistreact/src/Components/Todo.jsx
@@ -5,11 +5,11 @@ import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 export const Todo = ({ task, toggleComplete }) => {
     return (
-        <div className='Todo'>
-            <p onClick={() => toggleComplete(task.id)} className={`${task.completed ? 'completed':""}` }>{ task.task }</p>
+        <div className="Todo">
+            <p className={`${task.completed ? "completed" : "incompleted"}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
             <div>
-                <FontAwesomeIcon icon={faPenToSquare} />
-                <FontAwesomeIcon icon={faTrash} />
+                <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} onClick={() => editTodo(task.id)} />
+                <FontAwesomeIcon className="delete-icon" icon={faTrash} onClick={() => deleteTodo(task.id)} />
             </div>
         </div>
     )
